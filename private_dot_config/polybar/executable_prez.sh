05911a207406b37a46002e58acc823b1a404ca27
Dauliac <juliendauliac@gmail.com>
2020-04-20 10:49:46 +0200

    use xdg, zsh, and cool stuffs

--- private_dot_config/polybar/executable_prez.sh
+++
@@ -1,4 +0,0 @@
-PREZ_TOPIC="Lisez ca en attendant que je lance mon diapo"
-PREZ_USERS="Dauliac"
-
-echo "  ${PREZ_USERS} : ${PREZ_TOPIC} "
--- private_dot_config/polybar/executable_vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-vm=$(virsh list --state-running --name)
-if [ ! -z "$vm" ]; then
-    echo  $(virsh list --state-running --name | wc -l)
-else
-    echo ""
-fi
