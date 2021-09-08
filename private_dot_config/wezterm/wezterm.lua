018e1e3f685a13923548a66a7c5a1c1340f0c494
Dauliac <dauliac@pm.me>
2021-09-09 01:07:37 +0200

    Update private_dot_config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
 
-  color_scheme = "Batman",
+  color_scheme = "Gruvbox Dark",
   window_background_opacity = 0.8,
-  font = wezterm.font("FiraCode Nerd Font Mono", {weight = "Regular", stretch = "Normal", italic = false}),
+  font = wezterm.font("FiraCode Nerd Font Mono", {bold=true, weight = "Regular", stretch = "Normal", italic = false}),
 }
