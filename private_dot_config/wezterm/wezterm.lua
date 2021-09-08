2e74e01d1a0bf1e6241e5ebc6c2e3404aa3fa50f
Dauliac <dauliac@pm.me>
2021-09-09 01:02:40 +0200

    Add private_dot_config/wezterm/wezterm.lua

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+
+return {
+
+  color_scheme = "Batman",
+  window_background_opacity = 0.8,
+  font = wezterm.font("FiraCode Nerd Font Mono", {weight = "Regular", stretch = "Normal", italic = false}),
+}
