ee958b8d7a291b34aed5f9ccb5c51191d26ba21d
Dauliac <dauliac@pm.me>
2021-11-02 17:33:36 +0100

    Update private_dot_config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
   color_scheme = "Gruvbox Dark",
-  window_background_opacity = 0.8,
-  font = wezterm.font("FiraCode Nerd Font Mono", {bold=true, weight = "Regular", stretch = "Normal", italic = false}),
-  default_cursor_style = "SteadyBar",
+  window_background_opacity = 0.89,
+  font = wezterm.font("FiraCode Nerd Font Mono", {bold=false, weight = "Regular", stretch = "Normal", italic = false}),
+  default_cursor_style = "SteadyUnderline",
 }
