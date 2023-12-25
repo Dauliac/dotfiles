ea2d53c4c66c3d141999ff9fe5fbbcc101ffd939
jdauliac <jdauliac@sellsy.com>
2023-12-25 22:56:03 +0100

    feat: add some pets

--- modules/configs/pet.nix
+++ modules/configs/pet.nix
@@ -14,12 +14,49 @@
     {
       command =
         "ls $(nix build 'nixpkgs#typescript' --print-out-paths --no-link)";
+      description = "List typescript packages";
       tag = [ "nix" "package" ];
     }
     {
       command =
         "kubectl create secret generic regcred --from-file=.dockerconfigjson=/home/\${USER}/.docker/config.json --type=kubernetes.io/dockerconfigjsom --namespace sellsy-system";
+      description = "Create kubernetes secret from docker config";
       tag = [ "kubernetes" "secret" "docker" ];
+    }
+    {
+      command = "systemctl-tui";
+      description = "Systemctl tui";
+      tag = [ "systemd" "tui" ];
+    }
+    {
+      command = "lazygit";
+      description = "Git tui";
+      tag = [ "git" "tui" ];
+    }
+    {
+      command = "lazydocker";
+      description = "Docker tui";
+      tag = [ "docker" "tui" ];
+    }
+    {
+      command = "k9s";
+      description = "Kubernetes tui";
+      tag = [ "kubernetes" "tui" ];
+    }
+    {
+      command = "hyperfine";
+      description = "Benchmark";
+      tag = [ "benchmark" ];
+    }
+    {
+      command = "nix repl '<nixpkgs>'";
+      description = "Nix repl";
+      tag = [ "nix" "repl" ];
+    }
+    {
+      command = "sniffnet";
+      description = "Sniff network";
+      tag = [ "network" "sniff" ];
     }
   ];
 }
