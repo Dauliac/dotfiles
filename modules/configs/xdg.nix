72731e7705b3de2bb7015bb5ebb7866987699866
jdauliac <jdauliac@sellsy.com>
2023-12-26 00:24:31 +0100

    fix: add missings wezterm icons

--- modules/configs/xdg.nix
+++ modules/configs/xdg.nix
@@ -1,14 +1,4 @@
 { config, pkgs }: {
   enable = true;
-  desktopEntries = {
-    wezterm = {
-      name = "WezTerm";
-      genericName = "Termnial";
-      exec = "${pkgs.wezterm}/bin/wezterm";
-      terminal = false;
-      categories = [ "System" "TerminalEmulator" "Utility" ];
-      mimeType = [ "application/x-shellscript" ];
-      icon = "${pkgs.wezterm}/share/icons/hicolor/256x256/apps/wezterm.png";
-    };
-  };
+  mime.enable = true;
 }
