c0cbe2a44cf86ffcf029073af43fa5c0dd9739ee
Dauliac <dauliac@protonmail.com>
2024-04-09 19:51:54 +0200

    fix: bad rebase bug

--- modules/home-manager/desktop/gtk.nix
+++ modules/home-manager/desktop/gtk.nix
@@ -10,11 +10,6 @@
     name = "Bibata-Modern-Classic";
     size = 12;
   };
-  # xdg.configFile = {
-  #   "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-  #   "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-  #   "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  # };
   gtk = {
     enable = true;
     theme = {
--- modules/home-manager/profiles/dauliac.nix
+++ modules/home-manager/profiles/dauliac.nix
@@ -1,9 +1,9 @@
-{ ... }: {
+{config, ...}: {
   imports = [
     ../desktop
   ];
   xdg.mime.enable = true;
-  home.activation.setupEtc = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.setupEtc = config.lib.dag.entryAfter ["writeBoundary"] ''
     /run/current-system/sw/bin/systemctl start --user sops-nix
   '';
 }
