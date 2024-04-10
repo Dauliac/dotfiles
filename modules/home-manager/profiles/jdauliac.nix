1373fb6f5cfa2c0af5a3b149fd262fa73aa112d4
jdauliac <jdauliac@sellsy.com>
2024-04-10 15:32:49 +0200

    fix(home-manager): nixGl integration

--- modules/home-manager/profiles/jdauliac.nix
+++ modules/home-manager/profiles/jdauliac.nix
@@ -1,8 +1,35 @@
-{ config, ... }: {
+{ config
+, pkgs
+, ...
+}:
+let
+  nixGL = pkgs.nixgl.nixGLIntel;
+  nixGlWrapper = pkg:
+    pkgs.buildEnv {
+      name = "nixGL-${pkg.name}";
+      paths =
+        [ pkg ]
+        ++ (map
+          (bin:
+            pkgs.hiPrio (pkgs.writeShellScriptBin bin ''
+              exec -a "$0" "${nixGL}/bin/nixGLIntel" "${pkg}/bin/${bin}" "$@"
+            ''))
+          (builtins.attrNames (builtins.readDir "${pkg}/bin")));
+    };
+in
+{
   # WARN: set it to true segfault nautilus in non nixOs systems
   xdg.mime.enable = false;
   xdg.systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share" ];
   home.activation.setupEtc = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     /usr/bin/systemctl start --user sops-nix
   '';
+  home.packages = with pkgs; [
+    (nixGlWrapper pkgs.firefox-devedition)
+    # slack
+  ];
+  programs = {
+    wezterm.package = nixGlWrapper pkgs.wezterm;
+    # alacritty.package = nixGlWrapper pkgs.alacritty;
+  };
 }
