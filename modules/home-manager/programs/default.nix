8276bab7ace657f6433d65f99cfbf6bd6cac18b4
jdauliac <jdauliac@sellsy.com>
2024-04-10 15:33:21 +0200

    fix(home-manager): k9s style

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -23,13 +23,79 @@
     enableZshIntegration = true;
   };
   # NOTE: cheat sheet
-  programs.tealdeer = {enable = true;};
+  programs.tealdeer = { enable = true; };
   # NOTE: cheat sheet
   programs.navi = {
     enable = true;
     enableZshIntegration = true;
   };
-  programs.jq = {enable = true;};
-  programs.k9s = {enable = true;};
-  programs.alacritty = {enable = true;};
+  programs.jq = { enable = true; };
+  programs.k9s = {
+    enable = true;
+    settings = {
+      k9s = {
+        body.bgColor = "default";
+        ui.skin = "transparent";
+      };
+    };
+    skins = {
+      transparent = {
+        k9s = {
+          body = {
+            bgColor = "default";
+          };
+          prompt = {
+            bgColor = "default";
+          };
+          info = {
+            sectionColor = "default";
+          };
+          dialog = {
+            bgColor = "default";
+            labelFgColor = "default";
+            fieldFgColor = "default";
+          };
+          frame = {
+            crumbs = {
+              bgColor = "default";
+            };
+            title = {
+              bgColor = "default";
+              counterColor = "default";
+            };
+            menu = {
+              fgColor = "default";
+            };
+          };
+          views = {
+            charts = {
+              bgColor = "default";
+            };
+            table = {
+              bgColor = "default";
+              header = {
+                fgColor = "default";
+                bgColor = "default";
+              };
+            };
+            xray = {
+              bgColor = "default";
+            };
+            logs = {
+              bgColor = "default";
+              indicator = {
+                bgColor = "default";
+                toggleOnColor = "default";
+                toggleOffColor = "default";
+              };
+            };
+            yaml = {
+              colonColor = "default";
+              valueColor = "default";
+            };
+          };
+        };
+      };
+    };
+  };
 }
