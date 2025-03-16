fbd660e91c2d21a2c4bffdcb529bd0c38dc842df
Dauliac <dauliac@protonmail.com>
2025-03-16 23:25:44 +0100

    fix(hom-manager): set hyprpaper for all screens

--- modules/home-manager/desktop/wallpaper.nix
+++ modules/home-manager/desktop/wallpaper.nix
@@ -13,11 +13,7 @@ in
         path
       ];
       wallpaper = [
-        "eDP-1,${path}"
-        "DP-1,${path}"
-        "DP-2,${path}"
-        "DP-3,${path}"
-        "HDMI-A-1,${path}"
+        ",${path}"
       ];
     };
   };
