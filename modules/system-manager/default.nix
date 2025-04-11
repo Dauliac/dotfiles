b6ae4885a4496aff37958cfc85e3cd08879d717e
juliendauliac <dauliac@fake.io>
2025-04-11 11:51:01 +0200

    feat: manage docker service using system-manager

--- modules/system-manager/default.nix
+++ modules/system-manager/default.nix
@@ -1,3 +1,53 @@
-{...}: {
-
+{ lib, pkgs, ... }: {
+  config = {
+    nixpkgs.hostPlatform = "aarch64-linux";
+    environment = {
+      systemPackages = with pkgs; [
+        docker
+        apparmor-parser
+        apparmor-utils
+      ];
+    };
+    systemd.services = {
+      docker = {
+        enable = true;
+        description = "Docker Application Container Engine";
+        documentation = [ "https://docs.docker.com" ];
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        requires = [ "apparmor.service" ];
+        serviceConfig = {
+          Type = "notify";
+          Environment = [
+            "PATH=${lib.makeBinPath [
+              pkgs.docker
+              pkgs.apparmor-parser
+              pkgs.apparmor-utils
+              pkgs.coreutils
+              pkgs.kmod
+            ]}:/usr/bin:/sbin"
+          ];
+          ExecStart = "${pkgs.docker}/bin/dockerd";
+          ExecStartPost = [
+            "${pkgs.coreutils}/bin/chmod 666 /var/run/docker.sock"
+            "${pkgs.coreutils}/bin/chown root:docker /var/run/docker.sock"
+          ];
+          ExecReload = "${pkgs.coreutils}/bin/kill -s HUP $MAINPID";
+          TimeoutStartSec = 0;
+          RestartSec = 2;
+          Restart = "always";
+          StartLimitBurst = 3;
+          StartLimitInterval = "60s";
+          LimitNOFILE = "infinity";
+          LimitNPROC = "infinity";
+          LimitCORE = "infinity";
+          TasksMax = "infinity";
+          Delegate = true;
+          KillMode = "process";
+          OOMScoreAdjust = -500;
+        };
+      };
+    };
+  };
 }
